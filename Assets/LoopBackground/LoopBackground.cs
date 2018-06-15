f6045fabcea1696752caa4627253bd80366d6385
QZQ <928221136@qq.com>
2018-06-16 00:20:30 +0800

    Add basic Loop Background effect

---
+++ Assets/LoopBackground/LoopBackground.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoopBackground : MonoBehaviour {
+
+    private Image image;
+    private Material mtl;
+    float distance;
+
+    void Start () {
+        image = GetComponent<Image>();
+        mtl = image.material;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        distance += 0.005f;
+        mtl.SetFloat("_Distance", distance);
+    }
+}
